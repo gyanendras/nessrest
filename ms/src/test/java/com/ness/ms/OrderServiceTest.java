6d11111b7b9261e12fa6c3758cd6678302a1aedf
Gyanendra Singh <gyanendra.singh@congnizant.com>
2023-04-04 10:39:12 +0530

    Added Mock test of Order Service

--- ms/src/test/java/com/ness/ms/OrderServiceTest.java
+++ ms/src/test/java/com/ness/ms/OrderServiceTest.java
@@ -2,16 +2,30 @@ package com.ness.ms;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.ness.ms.domain.Order;
+import com.ness.ms.repo.OrderRepository;
 import com.ness.ms.service.OrderService;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class OrderServiceTest {
+	
+	@Mock
+	OrderRepository orderRepository;
 
+    @InjectMocks
+	OrderService mockOrderService;
 	
 	@Autowired
 	OrderService orderservice;
@@ -27,7 +41,8 @@ public class OrderServiceTest {
 	@Test 
 	void testGetOrder(){
 		
-		Order order = orderservice.getOrder(1L);
+		Mockito.when(orderRepository.getOrder(1L)).thenReturn(new Order(1l,1l,LocalDate.now(), 22.22, "InProcess"));
+		Order order = mockOrderService.getOrder(1L);
 		assertNotNull(order);
 		
 	}
